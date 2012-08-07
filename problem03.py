58064e2abdd4cf9cae947718f184f7a5a21cf12e
ugur <ugurdonmez87@gmail.com>
2012-08-07 22:15:24 +0300

    Project Euler Problem 03
    Problem solved effectively. It find biggest prime factor of the given
    number.

--- problem03.py
+++ problem03.py
@@ -1,22 +1,11 @@
 import math
 
 def primeFactor(n):
-	limit = n / 2
-	for i in reversed(xrange(2,limit)):
-		print(i)
-		if n % i == 0:
-			print(i)
-			if testPrime(i):
-				print(i)
-				break
-
-def testPrime(n):
-	limit = int(math.sqrt(n))
-	for i in range(2,limit+1):
-		if n % i == 0:
-			return 0
-	return 1
+	for i in xrange(2,math.sqrt(n)+1):
+		if i != n and n % i == 0:
+			n = n / i
+			i = i - 1
+	print(n)
 
 
-#primeFactor(10000000000)
 primeFactor(600851475143)
