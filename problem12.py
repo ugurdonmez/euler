cc6003721d8f14e943c09ddc6c4c52b191e1434e
ugurdonmez <donmez@metu.edu.tr>
2013-06-17 18:11:07 +0300

    Euler Project Problem 12
    What is the value of the first triangle number to have over five hundred divisors?

--- problem12.py
+++ problem12.py
@@ -1,16 +1,27 @@
 def factor_triangle_numbers():
     list = []
-    counter = 1
-    triangle_number = 2
+    counter = 2
+    triangle_number = 1
+    temp = triangle_number
+    power_counter = 0
     while True:
         triangle_number = triangle_number + counter
+        temp = triangle_number
         counter += 1
         list = []
-        for i in range (1, int(triangle_number / 2 + 1 ) ):
-            if triangle_number % i == 0:
-                list.append(i)
-                if len(list) >= 500:
-                    print triangle_number
-                    return 
+        i = 2
+        while i <= temp:
+            power_counter = 0
+            while temp % i == 0:
+                temp = temp / i
+                power_counter = power_counter + 1
+            power_counter = power_counter + 1
+            if power_counter > 1:
+                list.append(power_counter)
+            i = i + 1
+        if len(list) > 0 and reduce(lambda x, y: x * y, list) >= 500:
+            print triangle_number
+            return
 
 factor_triangle_numbers()
+
