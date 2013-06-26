2a48f09376c7820c2b0118ec44b108c5bb439c58
Ugur Donmez <donmez@metu.edu.tr>
2013-06-26 10:18:18 +0300

    Problem 18 Typo Correction

--- promlem18/problem18.py
+++
@@ -1,7 +0,0 @@
-table = [[int(n) for n in s.split()] for s in open('problem18.inp').readlines()]
- 
-for row in range(len(table)-1, 0, -1):
-  for col in range(0, row):
-    table[row-1][col] += max(table[row][col], table[row][col+1])
- 
-print table[0][0]
